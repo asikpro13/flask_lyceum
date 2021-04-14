21197a66b43feb38ef944e5268e83fcc3035d680
asikpro13 <asikpro13@mail.ru>
2021-04-14 23:05:03 +0500

    add test

--- data/test.py
+++ data/test.py
@@ -4,6 +4,7 @@ print(get('http://127.0.0.1:8080/api/jobs').json())  # Получение все
 print(get('http://127.0.0.1:8080/api/jobs/3').json())  # Получение одной работы
 print(get('http://127.0.0.1:8080/api/jobs/21д').json())  # Неверный id
 print(get('http://127.0.0.1:8080/api/jobs/sad').json())  # Строка
+
 print(post('http://127.0.0.1:8080/api/jobs/', json={
     'team_leader': '3',
     'job': 'ewd',
@@ -11,4 +12,13 @@ print(post('http://127.0.0.1:8080/api/jobs/', json={
     'collaborators': '1, 2, 3',
     'is_finished': True,
     'id_creator': 3}
-           ))
+           ))  # Корректный запрос
+
+print(post('http://127.0.0.1:8080/api/jobs/', json={}))  # Передан пустой запрос
+print(post('http://127.0.0.1:8080/api/jobs/', json={
+    'team_leader': '3',
+    'job': 'ewd',
+    'work_size': '1',
+    'collaborators': '1, 2, 3',
+    'id_creator': 3}))  # Не передан 1 параметр
+print(post('http://127.0.0.1:8080/api/jobs/'))  # Аргументы не переданы
